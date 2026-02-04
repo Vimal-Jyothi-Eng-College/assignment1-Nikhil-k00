308a1f4dcbef0068f838afe3a9b0db70f9453aa4
Nikhil k <nikhil82647@gmail.com>
2026-02-04 21:25:06 +0530

    commit

--- q3.py
+++ q3.py
@@ -1,15 +1,8 @@
-list=[]
-n=int(input("Enter the number of elements: "))
-for i in range(n):
-    val=int(input("Enter the number: "))
-    list.append(val)
-largest=list[0]
-for n in list:
-    if n>largest:
-        largest=n   
-print("The largest number is:",largest)
-
-
-
-
-
+numbers = [10, 25, 5, 40, 15]
+largest = numbers[0]
+i = 1
+while i < len(numbers):
+    if numbers[i] > largest:
+        largest = numbers[i]
+    i = i + 1
+print(largest)
